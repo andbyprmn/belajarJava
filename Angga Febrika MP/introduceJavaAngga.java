d62bb58d5990aed664994574902358c7faa2d6b7
Angga <anggafmp@gmail.com>
2019-01-11 15:30:34 +0700

    new

---
+++ Angga Febrika MP/introduceJavaAngga.java
@@ -0,0 +1,18 @@
+class introduceJava{
+
+    public static void main(String[]args){
+        System.out.println("              Introduce Java");
+        System.out.println("===================================");
+        System.out.println("Nama:Angga Febrika MP");
+        System.out.println("Jabatan:Big Data");
+        System.out.println("Kelas:Java");
+        System.out.println("===================================");
+
+    }
+}
+
+//Perbedaan Print,Println, dan Printf
+
+//Print : untuk mencetak tulisan pada karakter terakhir 
+//Println : untuk mencetak tulisan per line/perbaris
+//Printf : untuk mencetak tulisan terformat (misal string/integer)
